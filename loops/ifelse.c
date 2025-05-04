2a3b5ee797b8e3e44a70702e1de060bb96ab62a9
Amir2k25 <amirkalim2k14@gmail.com>
2025-05-04 10:45:42 +0100

    finding even odd number

---
+++ loops/ifelse.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+
+{
+int num;
+printf("please enter any number : ");
+scanf("%d",&num);
+if(num%2==0)
+{
+    printf(" number is even");
+
+}
+else{
+
+    printf("number is odd");
+
+}
+
+    return 0;
+
+}
